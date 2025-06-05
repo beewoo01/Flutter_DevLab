1c06eb5d5646134380732155afcf55fd4c6ff906
beewoo01@naver.com <>
2025-06-05 13:31:48 +0900

    Chore: split app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_dev_lab/hero/list_screen.dart';
+import 'package:hero/hero.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +17,37 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: ListScreen(),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ListScreen()),
+                );
+              },
+              child: Text("HERO"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
